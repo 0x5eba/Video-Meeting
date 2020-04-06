fb25970a0ceee15fc39964d5c5436a74ce65009e
0x5eba <sbiollo@gmail.com>
2020-04-07 00:46:07 +0200

    heroku

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 if(process.env.NODE_ENV==='production'){
 	app.use(express.static(__dirname+"/build"))
-	app.get("/", (req, res, next) => {
+	app.get("*", (req, res, next) => {
 		res.sendFile(path.join(__dirname+"/build/index.html"))
 	})
 }
