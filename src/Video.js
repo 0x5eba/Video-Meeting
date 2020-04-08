92cae16c7065a86bbd0115ec00482a5598687f9b
0x5eba <sbiollo@gmail.com>
2020-04-08 13:44:28 +0200

    save

--- src/Video.js
+++ src/Video.js
@@ -324,8 +324,27 @@ class Video extends Component {
 						}
 
 						//Add the local video stream
-						if (window.localStream !== undefined) {
+						if (window.localStream !== undefined && window.localStream !== null) {
 							connections[socketListId].addStream(window.localStream);
+						} else {
+
+							let silence = () => {
+								let ctx = new AudioContext(), oscillator = ctx.createOscillator();
+								let dst = oscillator.connect(ctx.createMediaStreamDestination());
+								oscillator.start();
+								return Object.assign(dst.stream.getAudioTracks()[0], {enabled: false});
+							}
+							
+							let black = ({width = 640, height = 480} = {}) => {
+								let canvas = Object.assign(document.createElement("canvas"), {width, height});
+								canvas.getContext('2d').fillRect(0, 0, width, height);
+								let stream = canvas.captureStream();
+								return Object.assign(stream.getVideoTracks()[0], {enabled: false});
+							}
+							
+							let blackSilence = (...args) => new MediaStream([black(...args), silence()]);
+							
+							connections[socketListId].addStream(blackSilence());
 						}
 					}
 				});
