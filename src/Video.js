a5dea6c3840390ca73f1b0e3b7f8cad9e2e60567
0x5eba <sbiollo@gmail.com>
2020-04-29 15:07:16 +0200

    save

--- src/Video.js
+++ src/Video.js
@@ -13,6 +13,8 @@ import StopScreenShareIcon from '@material-ui/icons/StopScreenShare';
 import CallEndIcon from '@material-ui/icons/CallEnd';
 import ChatIcon from '@material-ui/icons/Chat';
 
+import faker from "faker";
+
 import { message } from 'antd';
 import 'antd/dist/antd.css'
 
@@ -57,11 +59,17 @@ class Video extends Component {
 			messages: [],
 			message: "",
 			newmessages: 0,
+			askForUsername: true,
+			username: faker.internet.userName(),
 		}
 		connections = {}
 
 		this.addMessage = this.addMessage.bind(this)
 		this.getMedia()
+	}
+
+	askForInteractions = () => {
+
 	}
 
 	getMedia = async () => {
@@ -89,7 +97,7 @@ class Video extends Component {
 			screen: this.screen
 		}, () => {
 			this.getUserMedia()
-			this.connectToSocketServer()
+			// this.connectToSocketServer()
 		})
 
 		if (navigator.mediaDevices.getDisplayMedia) {
@@ -459,12 +467,6 @@ class Video extends Component {
 						} else {
 
 							console.log("addStream silence")
-							
-							console.log("ok1")
-
-							alert(1)
-
-							console.log("ok2")
 
 							let silence = () => {
 								let ctx = new AudioContext()
@@ -605,75 +607,102 @@ class Video extends Component {
 		})
 	}
 
+	handleUsername = (e) => {
+		this.setState({
+			username: e.target.value
+		})
+	}
+
+	connect = () => {
+		this.setState({
+			askForUsername: false,
+		}, () => {
+			this.connectToSocketServer()
+		})
+	}
+
 	render() {
 		return (
 			<div>
-				<div className="btn-down" style={{backgroundColor: "whitesmoke", color: "whitesmoke", textAlign: "center"}}>
-					<IconButton style={{ color: "#424242" }} onClick={this.handleVideo}>
-						{(this.state.video === true) ? <VideocamIcon /> : <VideocamOffIcon />}
-					</IconButton>
-
-					<IconButton style={{ color: "#f44336" }} onClick={this.handleEndCall}>
-						<CallEndIcon />
-					</IconButton>
-
-					<IconButton style={{ color: "#424242" }} onClick={this.handleAudio}>
-						{this.state.audio === true ? <MicIcon /> : <MicOffIcon />}
-					</IconButton>
-
-					{this.state.screenAvailable === true ?
-						<IconButton style={{ color: "#424242" }} onClick={this.handleScreen}>
-							{this.state.screen === true ? <ScreenShareIcon /> : <StopScreenShareIcon />}
-						</IconButton>
-					: null }
-					
-					<Badge badgeContent={this.state.newmessages} max={999} color="secondary" onClick={this.openChat}>
-						<IconButton style={{ color: "#424242" }} onClick={this.openChat}>
-							<ChatIcon />
-						</IconButton> 
-					</Badge>
-				</div>
-
-				<Modal show={this.state.showModal} onHide={this.closeChat} style={{zIndex: "999999"}}>
-					<Modal.Header closeButton>
-					<Modal.Title>Chat Room</Modal.Title>
-					</Modal.Header>
-					<Modal.Body style={{overflow: "auto", overflowY: "auto", height: "400px"}} >
-						{this.state.messages.length > 0 ? this.state.messages.map((item, index) => (
-							<div key={item.sender + item.data + index}>
-								<b>{item.sender}</b>
-								<p style={{ wordBreak: "break-all"}}>{item.data}</p>
-							</div>
-						)) : <p>No message yet</p>}
-					</Modal.Body>
-					<Modal.Footer className="div-send-msg">
-						<Input placeholder="Message" value={this.state.message} onChange={e => this.handleMessage(e)} />
-						<Button variant="contained" color="primary" onClick={this.sendMessage}>Send</Button>
-					</Modal.Footer>
-				</Modal>
-				
-				<div className="container"> 
-					<div style={{paddingTop: "20px"}}>
-						<Input value={window.location.href} disable="true"></Input>
-						<Button style={{ 
-							backgroundColor: "#3f51b5", 
-							color: "whitesmoke", 
-							marginLeft: "20px", 
-							marginTop: "10px",
-							width: "120px",
-							fontSize: "10px"}} onClick={this.copyUrl}>Copy invite link</Button>
+				{this.state.askForUsername === true ? 
+				<div>
+					<div style={{background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
+						textAlign: "center", margin: "auto", marginTop: "100px"}}>
+						<p style={{margin: 0, fontWeight: "bold", paddingRight: "50px"}}>Set your username</p>
+						<Input placeholder="Username" value={this.state.username} onChange={e => this.handleUsername(e)} />
+						<Button variant="contained" color="primary" onClick={this.connect} style={{ margin: "20px" }}>Connect</Button>
 					</div>
-					
-					<Row id="main" className="flex-container" style={{margin: 0, padding: 0}}>
-						<video id="my-video" ref={this.localVideoref} autoPlay muted style={{
-							borderStyle: "solid",
-							borderColor: "#bdbdbd",
-							margin: "10px", 
-							objectFit: "fill",
-							width: "100%", 
-							height: "100%"}}></video>
-					</Row>
 				</div>
+				:
+				<div>
+					<div className="btn-down" style={{backgroundColor: "whitesmoke", color: "whitesmoke", textAlign: "center"}}>
+						<IconButton style={{ color: "#424242" }} onClick={this.handleVideo}>
+							{(this.state.video === true) ? <VideocamIcon /> : <VideocamOffIcon />}
+						</IconButton>
+
+						<IconButton style={{ color: "#f44336" }} onClick={this.handleEndCall}>
+							<CallEndIcon />
+						</IconButton>
+
+						<IconButton style={{ color: "#424242" }} onClick={this.handleAudio}>
+							{this.state.audio === true ? <MicIcon /> : <MicOffIcon />}
+						</IconButton>
+
+						{this.state.screenAvailable === true ?
+							<IconButton style={{ color: "#424242" }} onClick={this.handleScreen}>
+								{this.state.screen === true ? <ScreenShareIcon /> : <StopScreenShareIcon />}
+							</IconButton>
+						: null }
+						
+						<Badge badgeContent={this.state.newmessages} max={999} color="secondary" onClick={this.openChat}>
+							<IconButton style={{ color: "#424242" }} onClick={this.openChat}>
+								<ChatIcon />
+							</IconButton> 
+						</Badge>
+					</div>
+
+					<Modal show={this.state.showModal} onHide={this.closeChat} style={{zIndex: "999999"}}>
+						<Modal.Header closeButton>
+						<Modal.Title>Chat Room</Modal.Title>
+						</Modal.Header>
+						<Modal.Body style={{overflow: "auto", overflowY: "auto", height: "400px"}} >
+							{this.state.messages.length > 0 ? this.state.messages.map((item, index) => (
+								<div key={item.sender + item.data + index}>
+									<b>{item.sender}</b>
+									<p style={{ wordBreak: "break-all"}}>{item.data}</p>
+								</div>
+							)) : <p>No message yet</p>}
+						</Modal.Body>
+						<Modal.Footer className="div-send-msg">
+							<Input placeholder="Message" value={this.state.message} onChange={e => this.handleMessage(e)} />
+							<Button variant="contained" color="primary" onClick={this.sendMessage}>Send</Button>
+						</Modal.Footer>
+					</Modal>
+					
+					<div className="container"> 
+						<div style={{paddingTop: "20px"}}>
+							<Input value={window.location.href} disable="true"></Input>
+							<Button style={{ 
+								backgroundColor: "#3f51b5", 
+								color: "whitesmoke", 
+								marginLeft: "20px", 
+								marginTop: "10px",
+								width: "120px",
+								fontSize: "10px"}} onClick={this.copyUrl}>Copy invite link</Button>
+						</div>
+						
+						<Row id="main" className="flex-container" style={{margin: 0, padding: 0}}>
+							<video id="my-video" ref={this.localVideoref} autoPlay muted style={{
+								borderStyle: "solid",
+								borderColor: "#bdbdbd",
+								margin: "10px", 
+								objectFit: "fill",
+								width: "100%", 
+								height: "100%"}}></video>
+						</Row>
+					</div>
+				</div>
+				}
 			</div>
 		)
 	}
