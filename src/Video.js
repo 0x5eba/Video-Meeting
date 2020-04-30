07c8bebb6b32aa1fdddfbf1cd6ebbbcabd61e91b
0x5eba <sbiollo@gmail.com>
2020-04-30 16:20:44 +0200

    save

--- src/Video.js
+++ src/Video.js
@@ -638,7 +638,7 @@ class Video extends Component {
 				{this.state.askForUsername === true ? 
 				<div>
 					<div style={{background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
-						textAlign: "center", margin: "auto", marginTop: "100px", justifyContent: "center"}}>
+						textAlign: "center", margin: "auto", marginTop: "50px", justifyContent: "center"}}>
 						<p style={{margin: 0, fontWeight: "bold", paddingRight: "50px"}}>Set your username</p>
 						<Input placeholder="Username" value={this.state.username} onChange={e => this.handleUsername(e)} />
 						<Button variant="contained" color="primary" onClick={this.connect} style={{ margin: "20px" }}>Connect</Button>
@@ -649,8 +649,8 @@ class Video extends Component {
 							borderStyle: "solid",
 							borderColor: "#bdbdbd",
 							objectFit: "fill",
-							width: "80%", 
-							height: "50%"}}></video>
+							width: "60%", 
+							height: "30%"}}></video>
 					</div>
 				</div>
 				:
