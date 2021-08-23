1d2a2e9dddc7f9b7b977559a56c20e1bcb62cd51
Ganesh J <52096566+Abiganesh@users.noreply.github.com>
2021-08-23 14:26:22 +0530

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -521,7 +521,7 @@ class Video extends Component {
 							<Row id="main" className="flex-container" style={{ margin: 0, padding: 0 }}>
 								<video id="my-video" ref={this.localVideoref} autoPlay muted style={{
 									borderStyle: "solid",borderColor: "#bdbdbd",margin: "10px",objectFit: "fill",
-									width: "80%",height: "100%"}}></video>
+									width: "70%",height: "100%"}}></video>
 							</Row>
 						</div>
 					</div>
